1173e6711dc433cfb872fce9e329312a8fc62551
Tobia Ochsner <tobia.ochsner@hotmail.com>
2025-03-17 17:35:23 +1300

    Fix typing of fallback numerical methods.

--- src/distributions/numerical_distributions_utils.jl
+++ src/distributions/numerical_distributions_utils.jl
@@ -7,7 +7,7 @@ A wrapper for the fit_mle function from Distributions.jl with
     This is based on the paper "Approximate Formula of Coefficient
     of Variation for Weibull Distribution" by S. Tanaka & M. Ichikawa.
 """
-function fit_mle(::Type{<:Weibull}, x::AbstractArray{<:Real})
+function Distributions.fit_mle(::Type{<:Weibull}, x::AbstractArray{<:Real})
     coefficient_of_variation = std(x) / mean(x)
     alpha0 = coefficient_of_variation^(-1.075)
     fit_mle(Weibull, x, alpha0=alpha0)
@@ -17,7 +17,7 @@ end
 A numerical bisection method to find the mode of a univariate
 LogitNormal distribution.
 """
-function mode(d::LogitNormal; tol::Real=1e-16, maxiter::Int=1000)
+function Distributions.mode(d::LogitNormal; tol::Real=1e-16, maxiter::Int=1000)
     logit(x) = log(x) - log1p(-x)
     f(x) = d.σ^2 * (2x - 1) + d.μ - logit(x)
 
@@ -59,6 +59,6 @@ end
 """
 A simple Monte Carlo estimator for the mean of a LogitNormal distribution.
 """
-function mean(d::LogitNormal; num_samples::Int=10_000)
+function Distributions.mean(d::LogitNormal; num_samples::Int=10_000)
     return mean(rand(d, num_samples))
 end
