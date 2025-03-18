d28a46289c440ac11c59b67a59a6d2baee0b4f42
Tobia Ochsner <tobia.ochsner@hotmail.com>
2025-03-18 18:30:43 +1300

    Fix bug in ESS calulcation when all topologies are the same.

--- src/utils/tree_ess.jl
+++ src/utils/tree_ess.jl
@@ -7,21 +7,19 @@ end
 function get_ess(cladified_trees::Vector{CladifiedTree})
     total_branch_lengths = [sum(values(tree.parameters)) for tree in cladified_trees]
     
+    total_branch_lengths_ess = Inf
     try
         total_branch_lengths_ess = get_ess(total_branch_lengths)
-    catch
-        total_branch_lengths_ess = Inf
-    end
+    catch end
     
     ccd = CCD1(cladified_trees)
     map_tree = point_estimate(ccd)
     distances_to_map_tree = robinson_foulds_distance.(cladified_trees, Ref(map_tree))
     
+    distances_to_map_tree_ess = Inf
     try
         distances_to_map_tree_ess = get_ess(distances_to_map_tree)
-    catch
-        distances_to_map_tree_ess = Inf
-    end
+    catch end
     
     return min(distances_to_map_tree_ess, total_branch_lengths_ess)
 end
